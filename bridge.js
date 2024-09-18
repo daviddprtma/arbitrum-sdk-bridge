6995e2d73e7dd93d4d79c2807d0b3c94d5dc0040
daviddprtma <davidchristianpratama@gmail.com>
2024-09-18 11:48:04 +0700

    done quest

--- bridge.js
+++ bridge.js
@@ -15,29 +15,32 @@ async function bridgeEth(parentSigner, childChainId) {
    * - Initialize the L2 provider with the Arbitrum Sepolia RPC endpoint.
    * - This provider will be used to interact with the Arbitrum network.
    */
-  const arb_Provider = "";
+  const arb_Provider = new ethers.providers.JsonRpcProvider(process.env.L2RPC);
 
   /**
    * Part C: Get the L2 Network Configuration
    * - Use the Arbitrum SDK to retrieve the L2 network configuration.
    * - This allows us to interact with the correct Arbitrum network, in this case, Arbitrum Sepolia.
    */
-  const l2network = "";
-  const ethBridger = "";
+  const l2network = await getArbitrumNetwork(childChainId);
+  const ethBridger = new EthBridger(l2network);
 
   /**
    * Part D: Deposit ETH to Arbitrum Sepolia
    * - Utilize the EthBridger to deposit ETH from Ethereum Sepolia to Arbitrum Sepolia.
    * - Here, we're depositing 0.005 ETH.
    */
-  const deposit_L2 = "";
+  const deposit_L2 = await ethBridger.deposit({
+    amount: ethers.utils.parseEther("0.005"), // Specify the amount of ETH to deposit
+    parentSigner, // Use the signer on the L1 network (Ethereum Sepolia)
+  });
 
   /**
    * Part E: Wait for Deposit Transaction to be Mined
    * - Wait for the transaction on Ethereum Sepolia to be confirmed.
    * - Log the transaction hash for reference.
    */
-  const ethDepositTxReceipt = "";
+  const ethDepositTxReceipt = await deposit_L2.wait();
 
   console.log(`Deposit initiated: ${ethDepositTxReceipt.transactionHash}`);
   console.log("Now we wait for L2 side of the transaction to be executed ⏳");
@@ -47,7 +50,9 @@ async function bridgeEth(parentSigner, childChainId) {
    * - The transaction will also need to be confirmed on Arbitrum Sepolia.
    * - We wait for the child transaction receipt to ensure the ETH has been successfully bridged.
    */
-  const l2Result = "";
+  const l2Result = await ethDepositTxReceipt.waitForChildTransactionReceipt(
+    arb_Provider
+  );
 
   if (l2Result.complete) {
     console.log(
@@ -69,8 +74,8 @@ async function main() {
    * - Initialize the provider for the L1 network (Ethereum Sepolia).
    * - Create a signer using the private key from .env
    */
-  const l1Provider = "";
-  const l1Signer = "";
+  const l1Provider = new ethers.providers.JsonRpcProvider(process.env.L1RPC);
+  const l1Signer = new ethers.Wallet(process.env.PRIVATE_KEY, l1Provider);
 
   // Chain ID for Arbitrum Sepolia
   const Arb_Sepolia_ID = 421614;
